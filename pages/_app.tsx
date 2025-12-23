0921b85ee006c13df3c5040613453bf07ff5158d
SONGDAM <hansongdam@hansongdam-ui-Macmini.local>
2025-12-23 19:16:59 +0900

    ci: add and test ci

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import Layout from '../components/layout/Layout'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Layout from '../components/layout/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <Layout>
-            <Head>
-                <link rel="stylesheet" as="style" crossOrigin="anonymous" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css" />
-                <link rel="icon" href="/favicon_mint_v2.png?v=3" type="image/png" />
-            </Head>
-            <Component {...pageProps} />
-        </Layout>
-    )
+  console.log('this is tripsoda kz');
+  return (
+    <Layout>
+      <Head>
+        <link
+          rel='stylesheet'
+          as='style'
+          crossOrigin='anonymous'
+          href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css'
+        />
+        <link rel='icon' href='/favicon_mint_v2.png?v=3' type='image/png' />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
-
+export default MyApp;
